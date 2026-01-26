56cc024e57c3ce5a0f2adbf63fdf6d48b1634acd
Steven Brookes <steven.brookes@talisman-innovations.com>
2026-01-26 15:41:57 +0000

    Fix deprecation

--- src/Ekreative/HealthCheckBundle/DependencyInjection/EkreativeHealthCheckExtension.php
+++ src/Ekreative/HealthCheckBundle/DependencyInjection/EkreativeHealthCheckExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class EkreativeHealthCheckExtension extends Extension
 {
